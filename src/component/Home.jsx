20304e2f28429c74f69b20183a9cd68f9ee8c22b
sotasunohara <sotasuno0175@gmail.com>
2020-07-21 13:54:57 +0900

    Dividerを使った

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,14 +3,32 @@ import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
 import { Container } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
-
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 const Home=()=>{
     return(    
         <Container>
-        <Typography component="h1" variant="h5">
-        Claimer Home
-        </Typography>
-        <Link href='/Signin'>ClaimerにSignin</Link>
+            <Typography component="h1" variant="h5">
+            Claimer Home
+            </Typography>
+            <Grid
+                Container
+                direction="row"
+                justify="center"
+                alignItems="center"
+            >
+                aaaa
+            </Grid>
+            <Divider />
+            <Box component="span" m={1}>
+                <Button color="secondary" variant="contained">
+                    こんにちは
+                </Button>
+            </Box>
+            <Divider />
+            <Link href='/Signin'>ClaimerにSignin</Link>
         </Container>
     );
 }
