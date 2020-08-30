6a034cd1ddb34b803e977eedafbf07e4559838f8
sotasunohara <sotasuno0175@gmail.com>
2020-08-31 00:10:32 +0900

    投稿用入力フォームを作成した。( 文章のみ )

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
-import { Container, Paper } from '@material-ui/core';
+import { Container, Paper, TextField } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
@@ -11,29 +11,51 @@ import SendIcon from '@material-ui/icons/Send';
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 
-const useStyles = makeStyles((theme) => ({
-    button: {
-      margin: theme.spacing(1),
-    },topPostMark:{
-        alignItems:'right'
-    },root: {
-        flexGrow: 1,
-    }
-}));
+
+let visibilityState='visible'
+
 
 const handleClick=(e)=>{
 
 }
-const handleClickPost=(e)=>{
-    
-    window.alert("post");
-}
-const Home=()=>{
+
+const Home=(props)=>{
+
+    const useStyles = makeStyles((theme) => ({
+        button: {
+          margin: theme.spacing(1),
+        },topPostMark:{
+            alignItems:'right'
+        },root: {
+            flexGrow: 1,
+        },submitButton:{
+            position:'absolute',
+            right:0,
+            bottom:0,
+            color:"lightblue",
+            visibility: visibilityState
+        },postPaper:{
+            position:'relative',
+            flexDirection: '',
+            justifyContent:'flex-end',
+            height:100,
+            margin:10,
+            padding:10
+        },postContent:{
+            position:'relative',
+            top:0
+        }
+    }));
+    const handleClickPost=(e)=>{
+        
+        
+    }
+
     const classes = useStyles();
     return(    
         <div className={classes.root}>
             <Grid container spacing={3}>
-                <Grid item xs={3} style={{height:'100%'}}>
+                <Grid item xs={0} sm={3} style={{height:'100%'}}>
                     <Paper>
                     <IconButton
                         variant="contained"
@@ -48,13 +70,30 @@ const Home=()=>{
 
                     
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} sm={6}>
                  
                 <Paper onClick={handleClickPost}
-                style={{flexDirection: '',justifyContent:'flex-end',height:50,margin:10}}>
-                    <span class="message">
-                    投稿しよう！    
-                    </span>
+                elevation={3}
+                className={classes.postPaper}
+                >
+                    <TextField
+                        id="post-text"
+                        placeholder="投稿しよう"
+                        fullWidth
+                        rows={3}
+                        multiline
+                        className={classes.postContent}
+                    />
+                    <IconButton
+                        variant="contained"
+                        color="primary"
+                        disableElevation
+                        className={classes.submitButton}
+                        onClick={handleClick}
+                        
+                    >
+                        <SendIcon/>
+                    </IconButton>
                 {/*
                 <IconButton
                     
@@ -68,13 +107,13 @@ const Home=()=>{
                     <SendIcon/>
                 </IconButton>*/
                 }
-            
+
                 </Paper>
-                <Paper style={{height:100,margin:10}}>
+                <Paper style={{margin:10,padding:10}}>
                     投稿内容
                 </Paper>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} sm={6}>
 
                 </Grid>
             </Grid>
