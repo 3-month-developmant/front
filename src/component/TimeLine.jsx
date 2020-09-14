23967e03fc5a2c6220c4ca4138ac56268357c923
sotasunohara <sotasuno0175@gmail.com>
2020-09-14 15:09:29 +0900

    タイムラインにリアクションアイコン追加

--- src/component/TimeLine.jsx
+++ src/component/TimeLine.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Container, makeStyles, Paper, TextField } from '@material-ui/core';
+import { Avatar, CardContent, Container, IconButton, makeStyles, Paper, TextField } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import { red } from '@material-ui/core/colors';
+import CardHeader from '@material-ui/core/CardHeader';
+import { MoreVert } from '@material-ui/icons';
+import CardActions from '@material-ui/core/CardActions';
+import Favorite from '@material-ui/icons/Favorite';
 
 const TimeLine=()=>{
     const sample=require('./sample_data.json');
@@ -9,26 +15,49 @@ const TimeLine=()=>{
             position:'relative',
             flexDirection: '',
             justifyContent:'flex-end',
-            height:100,
+            
             margin:10,
             padding:10,
             flexGrow:1
+        },avater:{
+            backgroundColor: red[500],
         }
-    }))
+    }));
     const classes=useStyles();
     let time_line=[];
     //time_line.push(<Grid>);
     for(let i=0;i<sample.length;++i){//タイムラインを結合する
-        time_line.push(<Paper
+        time_line.push(
+        <Card
             elevation={1}
-            className={classes.postPaper}
-        >
-                {sample[i].name}
+            className={classes.postPaper}>
+                {/*ユーザ名とアイコン*/}
+            <CardHeader 
+                avater={
+                    <Avatar aria-label="recipe" className={classes.avater}>T</Avatar>
+                }
+                action={
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                }
+                title={sample[i].name}
+                subheader={sample[i].date}
+            />
+            <CardContent
+>
+
                 <br/>
                 {sample[i].text}
                 <br/>
-                {sample[i].date}
-            </Paper>);
+                
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <Favorite />
+                </IconButton>
+            </CardActions>
+        </Card>);
     }
     //time_line.push(</Grid>);
     return time_line;
